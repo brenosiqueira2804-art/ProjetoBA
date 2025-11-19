387dbef7387d2294e38e0329a2451e174d0fcccb
brenosiqueira2804 <brenosiqueira2802@gmail.com>
2025-11-19 19:55:48 -0300

    Subindo atualizações do projeto

---
+++ Models/Pedido.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoBA.Models;
+
+public partial class Pedido
+{
+    public int Id { get; set; }
+
+    public int? UsuarioId { get; set; }
+
+    public int? ClienteId { get; set; }
+
+    public int? ProdutoId { get; set; }
+
+    public int? Codpedido { get; set; }
+
+    public string? Quantidade { get; set; }
+
+    public virtual Cliente? Cliente { get; set; }
+
+    public virtual Produto? Produto { get; set; }
+
+    public virtual Usuario? Usuario { get; set; }
+}
